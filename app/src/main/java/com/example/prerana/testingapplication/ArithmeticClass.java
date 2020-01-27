949643bbc8cd17e6e09952fe9a8651bb1887dc44
PreranaPandit <preranapandit07@gmail.com>
2020-01-27 10:43:18 +0545

    Arithmetic class added

---
+++ app/src/main/java/com/example/prerana/testingapplication/ArithmeticClass.java
@@ -0,0 +1,18 @@
+package com.example.prerana.testingapplication;
+
+public class ArithmeticClass {
+
+    public float add(float f, float s)
+    {
+        return f+s;
+    }
+    public float subtract(float f, float s)
+    {
+        return f-s;
+    }
+    public float divide(float f, float s)
+    {
+        return f/s;
+    }
+
+}
